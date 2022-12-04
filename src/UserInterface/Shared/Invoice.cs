3421b12b08df40bf37e3e11f3aec32cd18fb79b7
Josef Fürlinger <josef.fuerlinger@smartpoint.at>
2022-12-04 10:45:21 +0100

    introduce src folder

---
+++ src/UserInterface/Shared/Invoice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp.Shared
+{
+    class Invoice
+    {
+        public Guid Id { get; set; }
+        public DateTime? DateOfTreatment { get; set; }
+        public string NameOfInstituation { get; set; }
+        public decimal? Amount { get; set; }
+    }
+}
