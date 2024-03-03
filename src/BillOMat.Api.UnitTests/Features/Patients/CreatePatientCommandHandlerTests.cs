6d600cac6d6a4a8dc56d8e2855ec01be4ed5fe03
Josef Fürlinger <josef.fuerlinger@smartpoint.at>
2024-03-03 17:59:16 +0100

    add new test Handle_ShouldReturnNoValidationFailures_WhenEverythingIsOk

--- src/BillOMat.Api.UnitTests/Features/Patients/CreatePatientCommandHandlerTests.cs
+++ src/BillOMat.Api.UnitTests/Features/Patients/CreatePatientCommandHandlerTests.cs
@@ -21,7 +21,7 @@ namespace BillOMat.Api.Test.Features.Patients
 
         [Fact]
         [UnitTest]
-        public async Task Handle_Should_ReturnValidationFailure_WhenEmailIsNotUniqe()
+        public async Task Handle_ShouldReturnValidationFailure_WhenEmailIsNotUniqe()
         {
             // Arrange
             var command = new CreatePatient.Command()
@@ -55,6 +55,46 @@ namespace BillOMat.Api.Test.Features.Patients
             result.Match(
                 patientId => throw new Exception("A patient was created with a duplicate email address!"),
                 failures => failures.Should().ContainEquivalentOf(new ValidationFailure("Email", "Email is already in use!"))
+            );
+        }
+
+
+        [Fact]
+        [UnitTest]
+        public async Task Handle_ShouldReturnNoValidationFailures_WhenEverythingIsOk()
+        {
+            // Arrange
+            var command = new CreatePatient.Command()
+            {
+                FirstName = "Josef",
+                LastName = "Fürlinger",
+                Nickname = "Joe",
+                Email = "josef.fuerlinger@gmail.com"
+            };
+
+            _patientRepositoryMock
+                .Setup(
+                    x => x.IsEmailUniqueAsync(
+                        It.IsAny<string>(),
+                        It.IsAny<CancellationToken>()))
+                .ReturnsAsync(true);
+
+            _unitOfWorkMock
+                .Setup(x => x.PatientRepository)
+                .Returns(_patientRepositoryMock.Object);
+
+
+            var handler = new CreatePatient.Handler(
+                               unitOfWork: _unitOfWorkMock.Object,
+                               validator: new CreatePatient.CommandValidator());
+
+            // Act
+            var result = await handler.Handle(command, default);
+
+            // Assert
+            result.Match<object>(
+                patientId => patientId.Should().Be(0),
+                failures => failures.Should().BeEmpty()
             );
         }
     }
